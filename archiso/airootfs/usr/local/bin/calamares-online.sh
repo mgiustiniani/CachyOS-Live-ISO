7e8280839b75cf640b2c77dd0913a6ffb10f81f9
Mario Giustiniani <mariogiustiniani@gmail.com>
2026-06-24 12:22:58 +0200

    rename: synapse-calamares-next -> synapse-calamares (remove -next suffix)

--- archiso/airootfs/usr/local/bin/calamares-online.sh
+++ archiso/airootfs/usr/local/bin/calamares-online.sh
@@ -32,7 +32,7 @@ main() {
     local ISO_VERSION="$(cat /etc/version-tag)"
     echo "USING ISO VERSION: ${ISO_VERSION}"
 
-    sudo pacman -Sy --noconfirm synapse-calamares-next
+    sudo pacman -Sy --noconfirm synapse-calamares
 
     # Get Hardware Informations
     inxi -F > "$log"
